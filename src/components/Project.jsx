5a3d794bf38870f1cb775189b5bb6c18bef06592
Ayako <ayakonagane@gmail.com>
2025-08-29 16:08:56 +1200

    text fix

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -88,7 +88,7 @@ const Project = () => {
       rel="noopener noreferrer"
       className="flex-1 text-center px-4 py-2 rounded-full bg-gradient-to-r from-pink-500 to-yellow-500 text-white hover:scale-105 transform transition-transform duration-300"
     >
-      Project Preview
+      Preview
     </a>
   )}
 
